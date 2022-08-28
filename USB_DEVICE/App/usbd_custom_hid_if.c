f30837f6fdbd7945aa20ab8c2410a9e988ca1054
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-08-28 13:54:35 +0200

    USB files edited

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -91,7 +91,53 @@
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
 {
   /* USER CODE BEGIN 0 */
-  0x00,
+    /* joystick report */
+    0x05, 0x01,                    /* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x04,                    /* USAGE (Joystick) */
+    0xa1, 0x01,                    /* COLLECTION (Application) */
+    0x05, 0x01,                    /*   USAGE_PAGE (Generic Desktop) */
+    0x09, 0x01,                    /*   USAGE (Pointer) */
+    0xa1, 0x00,                    /*   COLLECTION (Physical) */
+    0x75, 0x10,                    /*     REPORT_SIZE (16) */
+    0x16, 0x01, 0x80,              /*     LOGICAL_MINIMUM (-32767) */
+    0x26, 0xff, 0x7f,              /*     LOGICAL_MAXIMUM (32767) */
+    0x09, 0x30,                    /*     USAGE (X) */
+    0x09, 0x31,                    /*     USAGE (Y) */
+    0x09, 0x32,                    /*     USAGE (Z) */
+    0x09, 0x35,                    /*     USAGE (Rz) */
+    0x95, 0x04,                    /*     REPORT_COUNT (4) */
+    0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */
+    0x15, 0x00,                    /*     LOGICAL_MINIMUM (0) */
+    0x26, 0xff, 0x7f,              /*     LOGICAL_MAXIMUM (32767) */
+    0x09, 0x33,                    /*     USAGE (Rx) */
+    0x09, 0x34,                    /*     USAGE (Ry) */
+    0x09, 0x36,                    /*     USAGE (slider) */
+    0x09, 0x37,                    /*     USAGE (dial) */
+    0x95, 0x04,                    /*     REPORT_COUNT (4) */
+    0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */
+    0xc0,                          /*   END_COLLECTION */
+    0x09, 0x39,                    /*   USAGE (Hat switch) */
+    0x15, 0x01,                    /*   LOGICAL_MINIMUM (1) */
+    0x25, 0x08,                    /*   LOGICAL_MAXIMUM (8) */
+    0x35, 0x00,                    /*   PHYSICAL_MINIMUM (0) */
+    0x46, 0x3b, 0x01,              /*   PHYSICAL_MAXIMUM (315) */
+    0x65, 0x14,                    /*   UNIT (Eng Rot:Angular Pos) */
+    0x75, 0x04,                    /*   REPORT_SIZE (4) */
+    0x95, 0x01,                    /*   REPORT_COUNT (1) */
+    0x81, 0x42,                    /*   INPUT (Data,Var,Abs, Null) */
+    0x75, 0x04,                    /*   REPORT_SIZE (4) */
+    0x95, 0x01,                    /*   REPORT_COUNT (1) */
+    0x81, 0x41,                    /*   INPUT (Cnst,Ary,Abs,Null) */
+    0x05, 0x09,                    /*   USAGE_PAGE (Button) */
+    0x19, 0x01,                    /*   USAGE_MINIMUM (Button 1) */
+    0x29, 0x20,                    /*   USAGE_MAXIMUM (Button 32) */
+    0x15, 0x00,                    /*   LOGICAL_MINIMUM (0) */
+    0x25, 0x01,                    /*   LOGICAL_MAXIMUM (1) */
+    0x75, 0x01,                    /*   REPORT_SIZE (1) */
+    0x95, 0x20,                    /*   REPORT_COUNT (32) */
+    0x55, 0x00,                    /*   UNIT_EXPONENT (0) */
+    0x65, 0x00,                    /*   UNIT (None) */
+    0x81, 0x02,                    /*   INPUT (Data,Var,Abs) */
   /* USER CODE END 0 */
   0xC0    /*     END_COLLECTION	             */
 };
@@ -124,7 +170,7 @@ extern USBD_HandleTypeDef hUsbDeviceFS;
 
 static int8_t CUSTOM_HID_Init_FS(void);
 static int8_t CUSTOM_HID_DeInit_FS(void);
-static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state);
+static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* buffer);
 
 /**
   * @}
@@ -169,15 +215,13 @@ static int8_t CUSTOM_HID_DeInit_FS(void)
 
 /**
   * @brief  Manage the CUSTOM HID class events
-  * @param  event_idx: Event index
-  * @param  state: Event state
+  * @param  buffer: pointer to data buffer
   * @retval USBD_OK if all operations are OK else USBD_FAIL
   */
-static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state)
+static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* buffer)
 {
   /* USER CODE BEGIN 6 */
-  UNUSED(event_idx);
-  UNUSED(state);
+  UNUSED(buffer);
 
   /* Start next USB packet transfer once data processing is completed */
   if (USBD_CUSTOM_HID_ReceivePacket(&hUsbDeviceFS) != (uint8_t)USBD_OK)
