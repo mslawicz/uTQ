3848a731ff68c9ca525ef8ac86b2fed1f10ce0b2
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-09-11 11:52:35 +0200

    HID device name changed

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -65,8 +65,8 @@
 #define USBD_VID     0x0483
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "C2030"
-#define USBD_PID_FS     0x5714
-#define USBD_PRODUCT_STRING_FS     "µTQ"
+#define USBD_PID_FS     0x5715
+#define USBD_PRODUCT_STRING_FS     "micro TQ"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
 
