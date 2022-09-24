b5715ad9be7587990b2e1dbc7d9f43a048d48fcb
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-09-24 15:42:07 +0200

    rotary encoder files added

---
+++ Core/Inc/rotary_encoder.h
@@ -0,0 +1,18 @@
+/*
+ * rotary_encoder.h
+ *
+ *  Created on: 24 wrz 2022
+ *      Author: marci
+ */
+
+#ifndef INC_ROTARY_ENCODER_H_
+#define INC_ROTARY_ENCODER_H_
+
+
+class RotaryEncoder
+{
+
+};
+
+
+#endif /* INC_ROTARY_ENCODER_H_ */
---
+++ Core/Src/rotary_encoder.cpp
@@ -0,0 +1,10 @@
+/*
+ * rotary_encoder.cpp
+ *
+ *  Created on: 24 wrz 2022
+ *      Author: marci
+ */
+
+#include "rotary_encoder.h"
+
+
